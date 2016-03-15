2f7ef8b013e9af2a8d5a37682ec3cb79eaf97f32
terryme007 <terryme007@live.com>
2016-03-15 13:11:28 +0800

    change

---
+++ js/index.js
@@ -0,0 +1,12 @@
+/**
+ * Created by terry on 2016/3/15.
+ */
+function more(index){
+    alert("no more!");
+    switch (index){
+        case 0:{break;}
+        case 1:{break;}
+        case 2:{break;}
+        default:{alert("error!");break;}
+    }
+}
