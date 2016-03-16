c97724efe2ab27d64a276c21421a9184b3f39972
terryme007 <terryme007@live.com>
2016-03-17 01:00:15 +0800

    change

--- js/index.js
+++ js/index.js
@@ -10,3 +10,24 @@ function more(index){
         default:{alert("error!");break;}
     }
 }
+
+function task(){
+    var l=document.body.clientWidth;
+    var mh=window.pageYOffset||document.documentElement.scrollTop||document.body.scrollTop||0;
+    var task_box_height=document.getElementById("task_box").style.height;
+    var h= document.body.scrollHeight;
+    l=(l-660)/2+"px";
+    mh=(mh+200)+"px";
+
+    document.getElementById("task_box").style.left=l;
+    document.getElementById("task_box").style.top=mh;
+
+    h=h+"px";
+    document.getElementById("bg_box").style.height=h;
+    $("#bg_box").show();
+    $("#task_box").show();
+}
+
+function to_page(index){
+    window.open("task/index.html?id="+index);
+}
